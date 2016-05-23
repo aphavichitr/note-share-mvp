cf7c6ccbbe5df542de4dde79da8904b23c80e92b
Andrew Phavichitr <andrew.phavichitr@gmail.com>
2016-05-23 14:50:55 -0700

    Create navigation and search

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,6 @@
+var Nav = ({props}) => (
+
+  <nav>
+    <Search />
+  </nav>
+);
---
+++ client/src/components/Search.jsx
@@ -0,0 +1,13 @@
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" />
+      </div>
+    );
+  }
+}
